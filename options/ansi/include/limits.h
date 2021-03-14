f7add9f69d6b59b2436ffb2e17b298d865eeee7f
ytret <ytretyakov18@gmail.com>
2021-03-15 01:49:38 +0300

    ansi limits.h: add defines from GCC's (8.2.0) limits.h
    The latter is shadowed by the former after commit
    0b3b409c3128d6ca07bafc04365d0ce5bb669e3e.

--- options/ansi/include/limits.h
+++ options/ansi/include/limits.h
@@ -3,6 +3,56 @@
 
 #define CHAR_BIT 8
 
+#if defined (__i386__)
+#ifndef LONG_MAX
+#define LONG_MAX __LONG_MAX__
+#endif
+
+#ifndef LONG_MIN
+#define LONG_MIN (-LONG_MAX - 1L)
+#endif
+
+#ifndef ULONG_MAX
+#define ULONG_MAX (LONG_MAX * 2UL + 1UL)
+#endif
+
+#ifndef INT_MAX
+#define INT_MAX __INT_MAX__
+#endif
+
+#ifndef SCHAR_MIN
+#define SCHAR_MIN (-SCHAR_MAX - 1)
+#endif
+
+#ifndef SCHAR_MAX
+#define SCHAR_MAX __SCHAR_MAX__
+#endif
+
+#ifndef UCHAR_MAX
+# if __SCHAR_MAX__ == __INT_MAX__
+#  define UCHAR_MAX (SCHAR_MAX * 2U + 1U)
+# else
+#  define UCHAR_MAX (SCHAR_MAX * 2 + 1)
+# endif
+#endif
+
+#ifdef __CHAR_UNSIGNED__
+# undef CHAR_MIN
+# if __SCHAR_MAX__ == __INT_MAX__
+#  define CHAR_MIN 0U
+# else
+#  define CHAR_MIN 0
+# endif
+# undef CHAR_MAX
+# define CHAR_MAX UCHAR_MAX
+#else
+# undef CHAR_MIN
+# define CHAR_MIN SCHAR_MIN
+# undef CHAR_MAX
+# define CHAR_MAX SCHAR_MAX
+#endif
+#endif /* __i386__ */
+
 #ifdef LONG_MAX
 # ifdef LONG_MAX == INT32_MAX
 #  define LONG_BIT 32
